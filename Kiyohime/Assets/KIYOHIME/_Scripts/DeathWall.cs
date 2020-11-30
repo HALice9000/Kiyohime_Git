3478e43527d31283960b1cdb01236512d9d98cd1
Valentin <valentin.paillot@orange.fr>
2020-11-30 18:55:50 +0100

    update: deathwall

---
+++ Kiyohime/Assets/KIYOHIME/_Scripts/DeathWall.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathWall : MonoBehaviour
+{
+    public Transform[] _checkpoints;
+    public float _speed = 10f;
+    int indexed = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        Vector3 dir = _checkpoints[indexed].position - transform.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+}
