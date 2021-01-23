19a5657c999f017cb7889d61e7ac873ce5e57594
Valentin <valentin.paillot@orange.fr>
2021-01-23 20:32:31 +0100

    Add: Dialogue System (need Updates)

---
+++ Kiyohime/Assets/KIYOHIME/_Scripts/DialogueSystem/s_dialogueSystem.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class s_dialogueSystem : MonoBehaviour
+{
+    public Queue<string> sentences;
+    [SerializeField] private TMP_Text _speakerName;
+    [SerializeField] private TMP_Text _speakerText;
+
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(s_dialogue dialogue)
+    {
+        sentences.Clear();
+
+        _speakerName.text = dialogue.name;
+
+        foreach (var sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if(sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        _speakerText.text = sentence;
+    }
+
+    private void EndDialogue()
+    {
+
+    }
+}
